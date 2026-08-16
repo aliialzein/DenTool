0c063d225971b055f2d3760adec4de1807e84206
ali alzein <aliialzein1@gmail.com>
2026-08-16 14:38:16 +0300

    Fixed auth module

--- apps/api/prisma/seed-admin.ts
+++ apps/api/prisma/seed-admin.ts
@@ -4,6 +4,9 @@
 import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import * as argon2 from 'argon2';
+import { config } from 'dotenv';
+
+config({ path: '.env.local' });
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
--- apps/api/src/modules/auth/auth.controller.spec.ts
+++ apps/api/src/modules/auth/auth.controller.spec.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/unbound-method */
 import { Test, TestingModule } from '@nestjs/testing';
 import { Response, Request } from 'express';
